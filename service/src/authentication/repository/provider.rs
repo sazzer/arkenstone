de0ff8e61af64fac5a4ca194d47de33fa38d6ffb
Graham Cox <graham@grahamcox.co.uk>
2020-04-21 07:48:53 +0100

    No-op mock provider for testing purposes

--- service/src/authentication/repository/provider.rs
+++ service/src/authentication/repository/provider.rs
@@ -6,3 +6,15 @@ pub trait ProviderCompleteAuth {}
 
 /// Trait that all Providers will implement
 pub trait Provider: ProviderStartAuth + ProviderCompleteAuth {}
+
+#[cfg(test)]
+pub struct MockProvider {}
+
+#[cfg(test)]
+impl ProviderStartAuth for MockProvider {}
+
+#[cfg(test)]
+impl ProviderCompleteAuth for MockProvider {}
+
+#[cfg(test)]
+impl Provider for MockProvider {}
